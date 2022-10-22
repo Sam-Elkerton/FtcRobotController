335b5e137140152908217578170638e3fd07de03
Sam-Elkerton <24elkertons@mtstcharles.org>
2022-10-22 16:19:31 -0400

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -15,34 +15,29 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
             DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
             DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
             DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
-
-            // Reverse the right side motors
-            // Reverse left motors if you are using NeveRests
-            motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-            motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
-
             waitForStart();
 
-            if (isStopRequested()) return;
+            motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            motorFrontLeft.setPower(0);
+            motorBackLeft.setPower(0);
+            motorFrontRight.setPower(0);
+            motorBackRight.setPower(0);
 
             while (opModeIsActive()) {
-                double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-                double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-                double rx = gamepad1.right_stick_x;
+                if(isStopRequested() == true){
+                    break;
+                }else{
+                    double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+                    double x = gamepad1.left_stick_x;
+                    double rx = gamepad1.right_stick_x;
 
-                // Denominator is the largest motor power (absolute value) or 1
-                // This ensures all the powers maintain the same ratio, but only when
-                // at least one is out of the range [-1, 1]
-                double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-                double frontLeftPower = (y + x + rx) / denominator;
-                double backLeftPower = (y - x + rx) / denominator;
-                double frontRightPower = (y - x - rx) / denominator;
-                double backRightPower = (y + x - rx) / denominator;
-
-                motorFrontLeft.setPower(frontLeftPower);
-                motorBackLeft.setPower(backLeftPower);
-                motorFrontRight.setPower(frontRightPower);
-                motorBackRight.setPower(backRightPower);
+                    motorFrontLeft.setPower(y + x + rx);
+                    motorBackLeft.setPower(y - x + rx);
+                    motorFrontRight.setPower(y - x - rx);
+                    motorBackRight.setPower(y + x - rx);
+                }
             }
         }
     }
